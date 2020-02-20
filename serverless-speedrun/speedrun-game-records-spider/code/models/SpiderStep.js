8e48de4c4e49fbe943ddd36767900e401470a824
ben7th <ben7th@sina.com>
2020-02-20 23:33:05 +0800

    移走 speedrun 爬虫代码

--- serverless-speedrun/speedrun-game-records-spider/code/models/SpiderStep.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const SpiderStep = new mongoose.Schema({
-  spiderName: String,
-  currentOffset: Number
-})
-
-module.exports = mongoose.model('SpiderStep', SpiderStep)
